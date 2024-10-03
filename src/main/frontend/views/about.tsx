7354330c0fcd3139e0775831ab6c9af690d1ba7b
iakovidva <va.iakovidis@gmail.com>
2024-10-03 14:28:42 +0200

    testing

--- src/main/frontend/views/about.tsx
+++ src/main/frontend/views/about.tsx
@@ -7,6 +7,7 @@ export default function AboutView() {
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
       <img style={{ width: '200px' }} src="images/empty-plant.png" />
       <h2>This place intentionally left empty</h2>
+        <p>sotos</p>
       <p>It’s a place where you can grow your own UI 🤗</p>
     </div>
   );
