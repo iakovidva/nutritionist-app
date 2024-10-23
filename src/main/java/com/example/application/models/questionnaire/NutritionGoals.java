6b1a12130e9ae9ab124d7f37e181430c073716d3
iakovidva <va.iakovidis@gmail.com>
2024-10-23 20:20:15 +0200

    map thymeleaf to objects

--- src/main/java/com/example/application/models/questionnaire/NutritionGoals.java
+++ src/main/java/com/example/application/models/questionnaire/NutritionGoals.java
@@ -1,10 +1,15 @@
 package com.example.application.models.questionnaire;
 
 import com.example.application.enums.Goals;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +22,12 @@ import lombok.NoArgsConstructor;
 @Entity(name = "NUTRITION_GOALS")
 public class NutritionGoals extends QuestionnaireSection {
 
+    @ElementCollection(targetClass = Goals.class)
     @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "nutrition_goal", joinColumns = @JoinColumn(name = "nutrition_goals_id"))
     @Column(name = "goal", nullable = false)
-    private Goals goals;
+    //TODO: consider how to structure that
+    private Set<Goals> goals = new HashSet<>();
 
     @Column(name = "timeframe_to_achieve")
     private String timeframeToAchieve;
